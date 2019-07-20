26fdb57de329dca786c7756b422e8437e6d98caf
Areeba Seher <46400505+Areeba-Seher04@users.noreply.github.com>
2019-07-20 01:17:55 -0700

    Update admin.py

--- NewsReader/NewsReader/News/admin.py
+++ NewsReader/NewsReader/News/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Channel,FChannel
 from django.contrib.auth.models import User
-#AreebaSeher
-#Genius1!Genius1!
 
 admin.site.register(Channel)
 
