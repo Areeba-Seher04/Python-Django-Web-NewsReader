92425018c82b90f355c47b9eda5372c8a76454d7
Areeba Seher <46400505+Areeba-Seher04@users.noreply.github.com>
2019-07-20 01:19:13 -0700

    Update urls.py

--- NewsReader/NewsReader/News/urls.py
+++ NewsReader/NewsReader/News/urls.py
@@ -10,16 +10,10 @@ urlpatterns = [
     path('<int:album_id>/', views.index , name='index'),
     path('list/', views.list , name='list'),
     path('detail/<int:album_id>/',views.detail,name='detail'),
-    #path('', views.index , name='index'),
     #News/register/
     path('register/', views.UserFromView.as_view() , name='register'),
     #/News/id/
-    #path('<int:channel_id>/', views.detail, name='detail'),
     path('delete/<int:channel_id>/',views.delete,name='delete'),
-
     path('login/',auth_views.LoginView.as_view(template_name='News/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='News/logout.html'),name='logout'),
-
-
-    
+    path('logout/',auth_views.LogoutView.as_view(template_name='News/logout.html'),name='logout'), 
     ]
